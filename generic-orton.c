3c82e541d7454ceb5e591790d87c76469faa23e2
LinuxBeaver <78667207+LinuxBeaver@users.noreply.github.com>
2022-05-12 04:14:41 +0000

    Add files via upload

---
+++ generic-orton.c
@@ -0,0 +1,89 @@
+/* This file is an image processing operation for GEGL
+ *
+ * GEGL is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * GEGL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GEGL; if not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright 2006 Øyvind Kolås <pippin@gimp.org>
+ */
+
+#include "config.h"
+#include <glib/gi18n-lib.h>
+
+#ifdef GEGL_PROPERTIES
+
+
+property_double (radius2, _("Radius"), 2.0)
+  value_range (0.0, 1000.0)
+  ui_range (0.0, 20.0)
+  ui_gamma (1.5)
+
+#else
+
+#define GEGL_OP_META
+#define GEGL_OP_NAME     generic_orton
+#define GEGL_OP_C_SOURCE generic-orton.c
+
+#include "gegl-op.h"
+
+static void attach (GeglOperation *operation)
+{
+  GeglNode *gegl = operation->node;
+  GeglNode *input, *output, *hl, *blur1, *blur2;
+
+  input    = gegl_node_get_input_proxy (gegl, "input");
+  output   = gegl_node_get_output_proxy (gegl, "output");
+
+  hl = gegl_node_new_child (gegl,
+                                  "operation", "gegl:hard-light",
+                                  NULL);
+
+  blur1    = gegl_node_new_child (gegl,
+                                  "operation", "gegl:gaussian-blur",
+                                  NULL);
+
+  blur2    = gegl_node_new_child (gegl,
+                                  "operation", "gegl:gaussian-blur",
+                                  NULL);
+
+  gegl_node_link_many (input, blur1, hl, output, NULL);
+  gegl_node_link (input, blur2);
+
+  gegl_node_connect_from (hl, "aux", blur2, "output");
+
+  gegl_operation_meta_redirect (operation, "radius1", blur1, "std-dev-x");
+  gegl_operation_meta_redirect (operation, "radius1", blur1, "std-dev-y");
+  gegl_operation_meta_redirect (operation, "radius2", blur2, "std-dev-x");
+  gegl_operation_meta_redirect (operation, "radius2", blur2, "std-dev-y");
+
+}
+
+static void
+gegl_op_class_init (GeglOpClass *klass)
+{
+  GeglOperationClass *operation_class;
+
+  operation_class = GEGL_OPERATION_CLASS (klass);
+
+  operation_class->attach = attach;
+
+  gegl_operation_class_set_keys (operation_class,
+    "name",        "gegl:generic-orton",
+    "title",       _("Generic Orton"),
+    "categories",  "Orton Effect",
+    "reference-hash", "45ed5656a238a5125700fc254001b2ac",
+    "description", _("Orton effect "
+                     ""),
+    NULL);
+}
+
+#endif
